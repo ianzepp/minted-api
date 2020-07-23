c38f79e2e3851017a0abe92a93ec820df644ff53
Ian Zepp <ian@ianzepp.com>
2020-07-23 15:29:48 -0400

    Reworking filter class to have better types

--- src/classes/filter.ts
+++ src/classes/filter.ts
@@ -3,14 +3,32 @@ import * as _ from 'lodash';
 import { Record } from '../classes/record';
 import { System } from '../classes/system';
 
-export interface FilterData {
-    where?: any[],
-    order?: any[],
+export type FilterOp = '$eq' | '$ne' | '$gt' | '$gte' | '$lt' | '$lte' | '$like' | '$nlike';
+
+export type FilterData = {
+    where?: Array<FilterWhereClause>,
+    order?: Array<FilterOrderClause>,
     limit?: number,
 }
 
-export class Filter extends Record {
-    constructor(system: System, schema_name: string) {
+export type FilterWhereClause = {
+    [index: string]: FilterWhereCriteria | {
+        [key in FilterOp]?: FilterWhereCriteria
+    };
+}
+
+export type FilterWhereCriteria = string | boolean | number | null;
+
+export type FilterOrderClause = {
+    [index: string]: 'asc' | 'desc';
+}
+
+export class Filter extends Record<FilterData> {
+    public readonly where: FilterWhereClause[] = [];
+    public readonly order: FilterOrderClause[] = [];
+    public limit: number = 0;
+
+    constructor(system: System, schema_name: string, readonly source?: FilterData) {
         super(system, schema_name);
     }
 }
