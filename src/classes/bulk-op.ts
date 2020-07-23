9c55155e8f7a104ba32f80d70fb107327f730058
Ian Zepp <ian@ianzepp.com>
2020-07-23 15:27:54 -0400

    Added dedicated file for BulkOp

---
+++ src/classes/bulk-op.ts
@@ -0,0 +1,17 @@
+export enum BulkOp {
+    CreateAll = 'create-all',
+    CreateOne = 'create-one',
+    Delete404 = 'delete-404',
+    DeleteAll = 'delete-all',
+    DeleteAny = 'delete-any',
+    DeleteOne = 'delete-one',
+    Select404 = 'select-404',
+    SelectAll = 'select-all',
+    SelectOne = 'select-one',
+    Update404 = 'update-404',
+    UpdateAll = 'update-all',
+    UpdateAny = 'update-any',
+    UpdateOne = 'update-one',
+    UpsertAll = 'upsert-all',
+    UpsertOne = 'upsert-one',
+};
