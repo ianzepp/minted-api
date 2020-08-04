bc0f9eceaf4bfd7519381bcd618f86fb670156ec
Ian Zepp <ian@ianzepp.com>
2020-08-04 12:23:10 -0400

    Deleting autoinstall placeholders from `src/` directory

--- mocha/autoinstall.spec.ts
+++ mocha/autoinstall.spec.ts
@@ -1,12 +1,16 @@
 import { expect } from 'chai';
 import 'mocha';
 
+// API
+import { System } from '../src/classes/system';
+
+// Script
 describe('Autoinstall', () => {
     it('Mocha sanity check', () => {
         expect(true).true;
     });
 
-    it('Install system tables', async () => {
-
+    it('Install schemas and columns', async () => {
+        let system = new System();
     });
 });
--- src/autoinstall/migration.ts
+++
@@ -1,5 +0,0 @@
-
-
-export abstract class Migration {
-    abstract async run(): Promise<any>;
-}
